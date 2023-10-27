dcfe11ed3065d6822089bcaad63eba4e3bd4a953
Christi Gilreath <130399626+christigilreath@users.noreply.github.com>
2023-10-27 12:18:37 -0400

    complete resistor-color

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,8 +3,29 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const colorCode = (color) => {
+  return COLORS.indexOf(color);
 };
 
-export const COLORS = undefined;
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+// - Black: 0
+// - Brown: 1
+// - Red: 2
+// - Orange: 3
+// - Yellow: 4
+// - Green: 5
+// - Blue: 6
+// - Violet: 7
+// - Grey: 8
+// - White: 9
